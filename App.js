56637a3731e8bcfadccbbf438555dd4b93fe118d
OMGDuke <lex1989@gmail.com>
2018-01-14 17:58:25 +0000

    Adding a profile card

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
+const profileImage = 'http://geekycentral.com/wp-content/uploads/2017/09/react-native.png'
+
 export default class App extends Component {
   render() {
     return (
-      <View >
-        <Image source={{uri: 'http://geekycentral.com/wp-content/uploads/2017/09/react-native.png'}}
-               style={styles.image} ></Image>
+      <View style={styles.card}>
+        <Image source={{uri: profileImage}}
+               style={styles.image} />
+        <View style={styles.blurb}>
+          <Text style={styles.name}>
+            Paul, 28
+          </Text>
+          <Text style={styles.job}>
+            Developer
+          </Text>
+        </View>
       </View>
     );
   }
@@ -14,6 +24,7 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   card: {
+    overflow: 'hidden',
     flex: 1,
     backgroundColor: 'white',
     margin: 10,
@@ -25,5 +36,14 @@ const styles = StyleSheet.create({
   },
   image: {
     flex: 1,
+  },
+  blurb: {
+    margin: 20,
+  },
+  name: {
+    fontSize: 20,
+  },
+  job: {
+    fontSize: 16
   }
 });
