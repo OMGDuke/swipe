c9698d9c82dcd97d33dcb9a554bdaa26f7c67de9
OMGDuke <lex1989@gmail.com>
2018-01-14 18:33:07 +0000

    Changed to expo app gen instead of create-react-native-app

--- App.js
+++ App.js
@@ -1,49 +1,21 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-const profileImage = 'http://geekycentral.com/wp-content/uploads/2017/09/react-native.png'
-
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.card}>
-        <Image source={{uri: profileImage}}
-               style={styles.image} />
-        <View style={styles.blurb}>
-          <Text style={styles.name}>
-            Paul, 28
-          </Text>
-          <Text style={styles.job}>
-            Developer
-          </Text>
-        </View>
+      <View style={styles.container}>
+        <Text>Hello World</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  card: {
-    overflow: 'hidden',
+  container: {
     flex: 1,
-    backgroundColor: 'white',
-    margin: 10,
-    marginTop: 100,
-    marginBottom: 100,
-    borderWidth: 1,
-    borderColor: 'lightgrey',
-    borderRadius: 8
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  image: {
-    flex: 1,
-  },
-  blurb: {
-    margin: 20,
-  },
-  name: {
-    fontSize: 20,
-  },
-  job: {
-    fontSize: 16
-  }
 });
